a505e5df93ffb91defe8d3f922b123f51ff3c6d4
mojdeh <mojdeh.rastgoo@gmail.com>
2017-01-30 22:11:21 +0100

    initialization of branch test

---
+++ CMakeFiles/pix2image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pix2image.dir/pix2image.o"
+  "pix2image.pdb"
+  "pix2image"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pix2image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
