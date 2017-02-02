395991a44dc80604d398a4a00c724c5b55c4d8ec
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2017-02-02 11:20:38 +0100

    Fixed error

--- pix2image.cpp
+++ pix2image.cpp
@@ -123,7 +123,9 @@ namespace POLPro
         cv::Point idmin, idmax;
         cv::minMaxLoc(img, &min, &max, &idmin, &idmax) ;
 
-        return "min max " + s + " : " << min << " " << max;
+        return "Image " + s
+            + ": min=" + std::to_string(min)
+            + " - max= " + std::to_string(max);
     }
 
     void imshow(std::vector<cv::Mat> img, const bool as_hsv=false,
@@ -147,73 +149,66 @@ namespace POLPro
                 img[2] = img[2] / 2;
             }
             // Convert to uint8
-            for (auto it = img.begin(); it != img.end(); ++it)
-                *it.convertTo(*it, CV_8UC1);
+            // for (auto it = img.begin(); it != img.end(); ++it)
+            //     *it.convertTo(*it, CV_8UC1);
+            for (int i = 0; i < img.size(); ++i)
+                img[i].convertTo(img[i], CV_8UC1);
         }
+
+        // Declare the output image
+        cv::Mat output_img;
 
         if (as_hsv) {
             // Merge the image together to have a 3 channels image
-            cv::Mat bgr_img, output_img;
+            cv::Mat bgr_img;
             cv::merge(img, bgr_img);
             cv::cvtColor(bgr_img, output_img, CV_HLS2BGR);
         } else {
             // Concatenate the images available together
             cv::Size img_size(img[0].cols, img[0].rows);
-            cv::Mat output_img(img[0].cols*2, img[0].rows * 2,
-                               CV_8UC1);
+            output_img = cv::Mat::zeros(img[0].cols*2, img[0].rows * 2,
+                                        CV_8UC1);
 
             for (int i = 0; i < img.size(); ++i) {
                 // we need to shift the image next to each other properly
                 int offset_row = i / 2;
                 int offset_col = i % 2;
                 img[i].copyTo(output_img(
-                                  cv::Rect(img_size.height() * offset_row,
-                                           img_size.width() * offset_col,
-                                           img_size.width(),
-                                           img_size.height())));
+                                  cv::Rect(img_size.height * offset_row,
+                                           img_size.width * offset_col,
+                                           img_size.width,
+                                           img_size.height)));
             }
         }
 
         cv::imshow("Output image", output_img);
         cv::waitKey(0);
-}
-
-					       
-int main( int argc, char** argv )
-{
-    if( argc != 2)
-    {
-	cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
-     return -1;
     }
+}  // Namespace POLPro
 
-    Mat image = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);   // Read the file
 
-    if(! image.data )                              // Check for invalid input
-    {
-        cout <<  "Could not open or find the image" << std::endl ;
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cout <<" Usage: display_image ImageToLoadAndDisplay" << std::endl;
         return -1;
     }
 
-    // parsed image from original image 
-    std::vector<cv::Mat> angle_image = POLPro::raw2mat(image, true); 
-    
-    // Stokes parameters 
+    cv::Mat image = cv::imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+
+    if (!image.data) {
+        std::cout <<  "Could not open or find the image" << std::endl;
+        return -1;
+    }
+
+    // parsed image from original image
+    std::vector<cv::Mat> angle_image = POLPro::raw2mat(image, true);
+
+    // Stokes parameters
     std::vector<cv::Mat> stokes_images = POLPro::compute_stokes(image, true);
-    
-    // polar componnets
-    std::vector<cv::Mat> polar_images = 
-	POLPro::compute_polar_params(stokes_images, true); 
 
-    //showing the paramters
+    // polar components
+    std::vector<cv::Mat> polar_images =
+        POLPro::compute_polar_params(stokes_images, true);
 
-    //POLPro::imshow(angle_image); 
-
-    //POLPro::imshow(stokes_images); 
-
-    //POLPro::imshow(polar_images, false, false); 
-    
-   waitKey(0); 
-   return 0 ; 
-   
+   return 0;
 }
